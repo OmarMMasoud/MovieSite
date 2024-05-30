fbcd8603e9b14df200a6616196e4c0a999eb5efa
OmarMMasoud <omarm.masoud@outlook.com>
2024-05-30 22:04:31 +0300

    bug fixes

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,28 +1,56 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
-import { IoSearch } from "react-icons/io5";
-import logo from "../style/imgs/logo.png"
-import "../style/nav.scss"
-
-function nav() {
+function Nav() {
   const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
   };
-  
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetchSearchResults();
+  };
+
+  const fetchSearchResults = () => {
+    const pages = [];
+    for (let i = 1; i <= 5; i++) {
+      pages.push(
+        axios.get(`http://www.omdbapi.com/?s=${searchTerm}&apikey=d40112b6&page=${i}`)
+      );
+    }
+
+    Promise.all(pages).then(responses => {
+      const movieData = [];
+      responses.forEach(response => {
+        const movies = response.data.Search;
+        if (movies) {
+          movies.forEach(movie => {
+            movieData.push({
+              title: movie.Title,
+              poster: movie.Poster,
+            });
+          });
+        } else {
+          console.error("Error: The 'Search' property is not present in the response data.");
+        }
+      });
+      setSearchResults(movieData);
+    }).catch(err => {
+      console.error(err);
+    });
+  };
+
   return (
     <nav>
-        <div className="logo"><img src={logo} alt="" /></div>
-        <div className="search">
-        <IoSearch className="search-icon" />
-
+      <form onSubmit={handleSubmit}>
         <input type="text" value={searchTerm} onChange={handleSearch} placeholder="Search..." />
-        <button onClick={() => /* Call the search function here */}>Search</button>
-
-
-        </div>
+        <button type="submit">Search</button>
+      </form>
     </nav>
-)
+  );
 }
 
-export default nav
+export default Nav;
